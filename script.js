ce2083119de78139c763713870500a092daa83ee
Guilherme Dias Sonza <114167270+Gui-Dias@users.noreply.github.com>
2024-02-05 10:07:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function soma() {
+    var num1 = document.getElementById("num1").valueasnumber ;
+    var num2 = document.getElementById("num2").valueasnumber ;
+    
+    var resultado = num1 + num2
+        document.getElementById("resultado").textContent= resultado ;
+        console.log(resultado)
+    
+
+}
